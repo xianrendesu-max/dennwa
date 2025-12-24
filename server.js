8f993cc4ed4fe69b27ad70c6a2766106acdbaea6
xianrendesu-max <xianrendesu@gmail.com>
2025-12-24 21:50:11 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
+const express = require("express");
+const http = require("http");
 const WebSocket = require("ws");
-const wss = new WebSocket.Server({ port: process.env.PORT || 3000 });
 
-const users = {}; // 電話番号 → ws
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+app.use(express.json());
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+const users = {};
 
 wss.on("connection", ws => {
-
   ws.on("message", msg => {
     const data = JSON.parse(msg);
 
@@ -32,4 +43,9 @@ wss.on("connection", ws => {
   ws.on("close", () => {
     if (ws.number) delete users[ws.number];
   });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });
